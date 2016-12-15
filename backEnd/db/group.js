0c7c35cfe1f6f3fd6a296049ebbe7e231b3153ff
bradyzhang <creatuture@gmail.com>
2016-12-15 17:32:48 +0800

    增加创建group的数据库操作

--- backEnd/db/group.js
+++ backEnd/db/group.js
@@ -4,14 +4,21 @@
 
 var pool = require('./dbPool');
 
-function createGroup (groupInfo, callback){
-  pool.getConnection (function (error, connection) {
-        if(!error){
-            connection.query('INSERT INTO `groups` VALUES(groupID,name,meme) ');
-        }else{
+function createGroup (groupInfo, callback) {
+    pool.getConnection(function (error, connection) {
+        if (!error) {
+            connection.query({
+                sql: 'INSERT INTO `groups` VALUES(groupId,name,userId,members,documents) VALUES(?,?,?,?,?)',
+                timeout: 3000,
+                values: [groupInfo.groupId, groupInfo.name, groupInfo.userId, groupInfo.members, groupInfo.documents]
+            }, function (error, result) {
+
+            });
+
+        } else {
             callback({
-                result : -1,
-                message : 'connect error'
+                result: -1,
+                message: 'connect error'
             });
         }
     });
@@ -19,5 +26,5 @@ function createGroup (groupInfo, callback){
 }
 
 module.exports = {
-    createGroup : createGroup
+    createGroup: createGroup
 };
