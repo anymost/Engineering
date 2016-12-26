1f5476bb02b3f0a195d9ad79af97084b0ed924b7
bradyzhang <creatuture@gmail.com>
2016-12-26 20:55:54 +0800

    构建前端框架

--- backEnd/db/redisCache.js
+++ backEnd/db/redisCache.js
@@ -4,6 +4,12 @@
 var redis = require('redis');
 var client = redis.createClient('6379', '127.0.0.1');
 
+/**
+ * @description 将手机号和对应的验证码存入redis中
+ * @param phone
+ * @param code
+ * @param callback
+ */
 var setPhoneCode = function (phone, code, callback){
     client.set('' + phone, '' + code, function (error, res){
         if(!error){
@@ -23,6 +29,11 @@ var setPhoneCode = function (phone, code, callback){
 exports.setPhoneCode = setPhoneCode;
 
 
+/**
+ * @description 通过手机号获取验证码
+ * @param phone
+ * @param callback
+ */
 var getPhoneCode = function (phone, callback) {
     client.get('' + phone, function (error, value) {
         if(!error){
--- frontEnd/src/main.js
+++ frontEnd/src/main.js
@@ -2,9 +2,7 @@
 
 import Vue from 'vue'
 import App from './App'
-import VueRouter from 'vue-router'
 
-Vue.use(VueRouter);
 
 
 new Vue({
