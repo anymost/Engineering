8f0dffbbefef19ef46d9df1896c1b0e0eee193c4
bradyzhang <creatuture@gmail.com>
2017-01-06 16:25:00 +0800

    修正部分场景的展示图片情况

--- frontEnd/src/components/container/NaviBar.vue
+++ frontEnd/src/components/container/NaviBar.vue
@@ -103,7 +103,8 @@
           if(this.stopChangeTab){
               return;
           }
-
+          this.$http.jsonp('http://localhost:3000/members',{name:'jack'}).
+            then((response)=>{console.log(response.data)},(error)=>{console.log('error '+JSON.stringify(error))});
           changeDisplay(event, true, this);
 
           store.dispatch('changeTitle');
